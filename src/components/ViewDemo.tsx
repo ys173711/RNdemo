61995691777517e05ac587e5ed8757e93a5c66bc
ys173711 <1737111795@qq.com>
2024-03-09 00:47:28 +0800

    RN组件, 继续学习...

--- src/components/ViewDemo.tsx
+++ src/components/ViewDemo.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 
 export default () => {
+  const inputRef = React.useRef<TextInput>(null);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setTimeout(() => {}, 3000);
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.root}>
-      <View style={[styles.subView]}>
-        <Text style={[styles.text]}>简体中文 111</Text>
-      </View>
-      <View style={[styles.subView, styles.subView_bgcolor_2]}>
-        <Text style={[styles.text2]}>简体中文 222</Text>
-      </View>
-      <View style={[styles.subView, styles.subView_bgcolor_3]}>
-        <Text style={[styles.text3]}>
-          简体中文 333
-          <Text style={[styles.box]}> box</Text>
-          <Text style={[styles.box]}> box2</Text>
-        </Text>
-      </View>
+      <TextInput
+        ref={inputRef}
+        style={styles.input}
+        // autoFocus={true}
+        blurOnSubmit={false}
+        caretHidden={false}
+        editable={true}
+        keyboardType="number-pad"
+        returnKeyType="go"
+        defaultValue="你好世界我是张三言叶"
+        // selection={{start: 2, end: 5}}
+        selectionColor="green"
+        // selectTextOnFocus={true}
+        secureTextEntry={true}
+      />
     </View>
   );
 };
@@ -25,52 +34,13 @@ const styles = StyleSheet.create({
   root: {
     flexDirection: 'row',
     height: '100%',
-    backgroundColor: 'lightgray',
-    justifyContent: 'center',
   },
-  subView: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'lightblue',
-    flexGrow: 1,
-  },
-  subView_bgcolor_2: {
-    backgroundColor: 'lightgreen',
-    flexGrow: 2,
-  },
-  subView_bgcolor_3: {
-    backgroundColor: 'lightcoral',
-    flexGrow: 3,
-  },
-  text: {
-    fontSize: 14,
-    fontWeight: 'normal',
-  },
-  text2: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    fontFamily: 'impact',
-    backgroundColor: 'lightyellow',
-  },
-  text3: {
-    fontSize: 14,
-    fontWeight: 'normal',
-    fontFamily: 'impact',
-    textAlign: 'center',
-    backgroundColor: 'lightgreen',
-    height: 40,
-    textAlignVertical: 'center',
-    textDecorationStyle: 'dotted',
-    textDecorationLine: 'underline',
-    textShadowColor: '#808080',
-    textShadowOffset: {width: 2, height: 4},
-    textShadowRadius: 4,
-  },
-  box: {
-    fontSize: 14,
-    fontWeight: 'normal',
-    fontFamily: 'impact',
-    backgroundColor: 'lightyellow',
-    marginTop: 10,
+  input: {
+    width: '100%',
+    height: 44,
+    borderWidth: 1,
+    backgroundColor: 'white',
+    color: 'red',
+    fontSize: 20,
   },
 });
